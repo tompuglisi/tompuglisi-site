c215c4d8eab60dcfd02fe9f53e10143e090984d6
Thomas Puglisi <tom.puglisi@gmail.com>
2018-01-02 21:13:15 -0600

    Adding sql file to insert quotes into the database

---
+++ InsertQuotes.sql
@@ -0,0 +1,9 @@
+START TRANSACTION;
+USE `tompuglisi`;
+INSERT INTO `tompuglisi`.`quotes` (`id`, `quote`, `author`) VALUES (1, 'Don\'t confuse genuine peace of mind with the passing lack of angst that follows the demise of a dream but precedes a mounting wave of regret.', 'NULL');
+INSERT INTO `tompuglisi`.`quotes` (`id`, `quote`, `author`) VALUES (2, 'Only people who don\'t work make no mistakes.', 'NULL');
+INSERT INTO `tompuglisi`.`quotes` (`id`, `quote`, `author`) VALUES (3, 'Music gives a soul to the universe, wings to the mind, flight to the imagination, and life to everything.', 'Plato');
+INSERT INTO `tompuglisi`.`quotes` (`id`, `quote`, `author`) VALUES (4, 'Be who you are and say what you feel, because those who mind don\'t matter and those who matter don\'t mind.', 'Dr. Seuss');
+INSERT INTO `tompuglisi`.`quotes` (`id`, `quote`, `author`) VALUES (5, 'Happiness is an inside job.', 'NULL');
+
+COMMIT;
