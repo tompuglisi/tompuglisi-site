263f3c0317b37680adc318ef737f71b8c3ece0d9
Thomas Puglisi <tom.puglisi@gmail.com>
2018-01-16 22:05:47 -0600

    Adding a simple favicon

--- views/templates/header.php
+++ views/templates/header.php
@@ -5,6 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="">
 <meta name="author" content="Tom Puglisi">
+<link rel="icon" href="favicon.ico">
 
 <title><?php echo htmlspecialchars($title) ?></title>
 
