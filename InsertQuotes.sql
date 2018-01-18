468c48da14939b3e06fc26b05b0a5f8df8c21975
Thomas Puglisi <tom.puglisi@gmail.com>
2018-01-17 22:00:11 -0600

    Updating quote population script to add null authors correctly

--- InsertQuotes.sql
+++ InsertQuotes.sql
@@ -1,10 +1,10 @@
 START TRANSACTION;
 USE `tompuglisi`;
-INSERT INTO `tompuglisi`.`quotes` (`id`, `quote`, `author`) VALUES (1, 'Don\'t confuse genuine peace of mind with the passing lack of angst that follows the demise of a dream but precedes a mounting wave of regret.', 'NULL');
+INSERT INTO `tompuglisi`.`quotes` (`id`, `quote`, `author`) VALUES (1, 'Don\'t confuse genuine peace of mind with the passing lack of angst that follows the demise of a dream but precedes a mounting wave of regret.', NULL);
 INSERT INTO `tompuglisi`.`quotes` (`id`, `quote`, `author`) VALUES (2, 'The only man who never makes a mistake is the man who never does anything.', 'Theodore Roosevelt');
 INSERT INTO `tompuglisi`.`quotes` (`id`, `quote`, `author`) VALUES (3, 'Music gives a soul to the universe, wings to the mind, flight to the imagination, and life to everything.', 'Plato');
 INSERT INTO `tompuglisi`.`quotes` (`id`, `quote`, `author`) VALUES (4, 'Be who you are and say what you feel, because those who mind don\'t matter and those who matter don\'t mind.', 'Dr. Seuss');
-INSERT INTO `tompuglisi`.`quotes` (`id`, `quote`, `author`) VALUES (5, 'Happiness is an inside job.', 'NULL');
+INSERT INTO `tompuglisi`.`quotes` (`id`, `quote`, `author`) VALUES (5, 'Happiness is an inside job.', NULL);
 INSERT INTO `tompuglisi`.`quotes` (`id`, `quote`, `author`) VALUES (6, 'The whole problem with the world is that fools and fanatics are always so certain of themselves, and wiser people so full of doubts.', 'Bertrand Russell');
 INSERT INTO `tompuglisi`.`quotes` (`id`, `quote`, `author`) VALUES (7, 'Don\'t be so humble - you are not that great.', 'Golda Meir');
 
